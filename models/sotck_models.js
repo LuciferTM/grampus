d43e1cf48f45722a090e9d535e99fccf4e8c6c40
LuciferTM <roy_qian@gmail.com>
2016-12-08 15:13:10 +0800

    add QuarterlyReport Schema

--- models/sotck_models.js
+++ models/sotck_models.js
@@ -4,7 +4,7 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+var ObjectId  = Schema.ObjectId;
 
 var Stock = new Schema({
     code: {type: String},
@@ -15,14 +15,30 @@ var Stock = new Schema({
     totalShares: {type: Number},
     pe_lyr: {type: Number},
     pe_ttm: {tyep: Number},
+    quarterly_report: [QuarterlyReport],
     exchange: {type: String} //市场
 });
 
+//季度报表
+var QuarterlyReport = new Schema({
+    stock_id: {type: ObjectId},
+    date: {type: Date},
+    profit:{type: Number},
+    revenue:{type: Number},
+    net_estate:{type: Number}
+});
+
+QuarterlyReport.index({stock_id: 1});
+
 var StockHistoryPrice = new Schema({
-    code: {type:String},
+    stock_id: {type: ObjectId},
     date: {type:Date},
     //需要考虑除权
     high_price: {type:Number},
     low_price: {type:Number}
 });
+
+StockHistoryPrice.index({sotck_id: 1});
+
+
 
