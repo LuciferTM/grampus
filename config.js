895aa63f6a40f8a0ba17c2e14d080696a063da02
LuciferTM <roy_qian@gmail.com>
2016-12-08 16:22:51 +0800

    config

---
+++ config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by www on 2016/12/8.
+ */
+
+
+var config = {
+    debug: true,
+    host:'localhost',
+    prot:3000,
+    db:'mongodb://127.0.0.1/grampus_dev'
+};
+
+module.exports = config;
+
