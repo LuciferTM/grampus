0161ced6308ff1b24fa613c408089a40fb7c74b1
LuciferTM <roy_qian@gmail.com>
2016-12-08 10:42:43 +0800

    add StockHistoryPrice

--- models/sotck_models.js
+++ models/sotck_models.js
@@ -5,14 +5,24 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
 var Stock = new Schema({
     code: {type: String},
+    market: {type: String},
     name: {tyep: String},
     current: {type: Number},
     marketCapital: {type: Number},
-    totalShares: {type:Number},
-    pe_lyr:{type:Number},
-    pe_ttm:{tyep:Number},
-    exchange:{type:String}, //市场
+    totalShares: {type: Number},
+    pe_lyr: {type: Number},
+    pe_ttm: {tyep: Number},
+    exchange: {type: String} //市场
+});
+
+var StockHistoryPrice = new Schema({
+    code: {type:String},
+    date: {type:Date},
+    //需要考虑除权
+    high_price: {type:Number},
+    low_price: {type:Number}
 });
 
