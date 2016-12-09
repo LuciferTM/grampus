8da1be02890cb09df69f273f13d9e3cb3b40b7d6
LuciferTM <roy_qian@gmail.com>
2016-12-09 10:10:59 +0800

    获取sotck列表接口

--- api/v1/stock.js
+++ api/v1/stock.js
@@ -2,11 +2,21 @@
  * Created by www on 2016/12/8.
  */
 var models     = require('../../models');
+var eventproxy = require('eventproxy');
+var _          = require('lodash');
 var StockModel = models.Stock;
 
 var index = function (req, res, next) {
     var query = {};
-    res.send({statu:200, message:"success!"});
+    var ep = new eventproxy();
+    ep.fail(next);
+    StockModel.find(query,ep.done('stocks'));
+    ep.all('stocks', function (stocks) {
+        stocks = stocks.map(function (stock) {
+           return _.pick(stock, ['code','market','name'])
+        });
+        res.send({success:true,data:stocks});
+    });
 };
 
 exports.index=index;
