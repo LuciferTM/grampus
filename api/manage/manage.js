3311c0e154aba0446dda539b51ffa7d31c90f417
LuciferTM <roy_qian@gmail.com>
2016-12-13 16:27:57 +0800

    superagent增加cookie使得可以调用雪球的接口

--- api/manage/manage.js
+++ api/manage/manage.js
@@ -9,11 +9,22 @@
  *
  */
 var superagent = require("superagent");
-var init = function () {
+var init = function (req, res, next) {
     superagent.get("http://xueqiu.com/v4/stock/quote.json")
+        .set("Cookie", "webp=0; bid=6b9bc245dbc572d045e62733d93aa954_ir4v9yzn; " +
+            "s=801kiyyr2f; xq_a_token=3b639312a44c89f74b919eb79bddf95099e116df; " +
+            "xqat=3b639312a44c89f74b919eb79bddf95099e116df; xq_r_token=cd2117aa89e99d6010efba2919c8e2403c8237ef; " +
+            "xq_is_login=1; u=5398755816; xq_token_expire=Thu%20Jan%2005%202017%2018%3A38%3A19%20GMT%2B0800%20(CST); " +
+            "snbim_minify=true; __utmt=1; Hm_lvt_1db88642e346389874251b5a1eded6e3=1481546654,1481595930,1481597502,1481615102; " +
+            "Hm_lpvt_1db88642e346389874251b5a1eded6e3=1481615112; __utma=1.1179963521.1468469551.1481595930.1481615106.381; __utmb=1.2.10.1481615106; " +
+            "__utmc=1; __utmz=1.1481595930.380.8.utmcsr=baidu|utmccn=")
         .query({code:"SZ000651"})
         .end(function (err, res) {
-            console.log(res)
+            if (err) {
+                console.log("err!")
+            }
+            console.log(res.text);
         });
+    res.send({"result":"success"});
 };
 exports.init = init;
